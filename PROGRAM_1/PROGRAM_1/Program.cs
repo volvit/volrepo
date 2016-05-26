2ee44c4f983b1a3b07255f3192f5367caa07e1e2
volvit <volv@1-vc.ru>
2016-05-26 18:20:40 +0300

    цвета

---
+++ PROGRAM_1/PROGRAM_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROGRAM_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleColor prevColor = Console.BackgroundColor;
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.ReadLine();
+
+        }
+    }
+}
